388ebf2e47ebf57d47b53dc35cfd33536b86e0fe
Homan <homanchou@gmail.com>
2021-10-28 22:19:18 -0700

    v0.0.3

--- priv/repo/migrations/20211021005023_create_entities.exs
+++ priv/repo/migrations/20211021005023_create_entities.exs
@@ -8,6 +8,7 @@ defmodule Thexr.Repo.Migrations.CreateEntities do
       add :type, :string, null: false
       add :space_id, references(:spaces, on_delete: :delete_all, type: :binary_id), null: false
       add :parent_id, references(:entities, on_delete: :delete_all, type: :binary_id)
+      add :child_count, :integer, default: 0, null: false
       # left
       # right
       # group_id
