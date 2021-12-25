2112a65c913e69700d227099c950881ac6330cc9
Homan <homanchou@gmail.com>
2021-12-24 23:27:41 -0800

    add some styling

--- lib/thexr_web/controllers/space_controller.ex
+++ lib/thexr_web/controllers/space_controller.ex
@@ -2,10 +2,11 @@ defmodule ThexrWeb.SpaceController do
   use ThexrWeb, :controller
   plug :put_root_layout, "xr_root.html"
 
-  alias Thexr.Spaces
+  plug :put_layout,
+       alias(Thexr.Spaces)
 
   def show(conn, %{"slug" => slug}) do
     space = Spaces.get_space_by_slug(slug)
-    render(conn, "show.html", space: space)
+    render(conn, "show.html", space: space, layout: false)
   end
 end
