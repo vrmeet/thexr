aca094a70f2806eb96799df250775dfa44a2de5f
Homan Chou <homanchou@gmail.com>
2022-01-08 17:26:48 -0800

    setup prod deploy for gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,19 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :thexr, ThexrWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  # Possibly not needed, but doesn't hurt
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :thexr, Thexr.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  pool_size: 2
 
 # Do not print debug messages in production
 config :logger, level: :info
