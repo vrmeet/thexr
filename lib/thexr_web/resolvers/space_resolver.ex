0aa3bdbdfde4320bb4230271f0f47401020eec87
Homan Chou <homanchou@gmail.com>
2022-02-08 17:44:19 -0800

    session 63: fix no mic permission error and add more functionality to graphql

--- lib/thexr_web/resolvers/space_resolver.ex
+++ lib/thexr_web/resolvers/space_resolver.ex
@@ -4,4 +4,12 @@ defmodule ThexrWeb.Resolvers.SpaceResolver do
   def spaces(_root, _args, _info) do
     {:ok, Spaces.list_spaces()}
   end
+
+  def space(_root, args, _info) do
+    {:ok, Spaces.get_space_by_slug(args.slug)}
+  end
+
+  def components(_, args, _) do
+    {:ok, Spaces.list_components_for_entity(args.entity_id)}
+  end
 end
