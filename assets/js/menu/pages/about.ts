f3eab280e41545f7a979615172327a36d22ad0f9
Homan Chou <homanchou@gmail.com>
2022-02-17 19:32:35 -0800

    parent mesh gui menu to left controller

--- assets/js/menu/pages/about.ts
+++ assets/js/menu/pages/about.ts
@@ -2,12 +2,12 @@ import type * as BABYLON from 'babylonjs'
 import * as GUI from 'babylonjs-gui'
 
 import '../helpers'
-import { g, a } from '../helpers';
+import { div, g, a } from '../helpers';
 
 export class MenuPageAbout extends GUI.Container {
     constructor() {
         super()
-        this.addControl(g(GUI.Rectangle, {},
+        this.addControl(div({ name: "about_div" },
             a({ target: "main" }, "< Back"),
             "Hello About Me"))
     }
