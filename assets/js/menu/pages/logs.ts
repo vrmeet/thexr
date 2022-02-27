a06c1c7ebb61bbcb3208542b9ffce7425a348704
Homan Chou <homanchou@gmail.com>
2022-02-27 10:18:27 -0800

    some better formatting for logs

--- assets/js/menu/pages/logs.ts
+++ assets/js/menu/pages/logs.ts
@@ -7,15 +7,24 @@ import { pre, a, div, g } from '../helpers';
 export class MenuPageLogs extends GUI.Container {
     constructor(public logManager: LogManager) {
         super()
+        this.name = "menu_page_logs"
+        this.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT
+        this.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP
         this.addControl(div({ name: "logs_div" },
             a({ menu_action: { name: "goto_main" } }, "< Back"),
-            pre({}, this.getLogs())
+            pre({ name: "logs_scroll_view" }, this.getLogs())
         )
         )
     }
 
     getLogs() {
-        return g(GUI.TextBlock, { width: "500px", height: "900px", text: this.logManager.recentLogsAsText() })
+        return g(GUI.TextBlock, {
+            width: 3,
+            textHorizontalAlignment: GUI.Control.HORIZONTAL_ALIGNMENT_LEFT,
+            textVerticalAlignment: GUI.Control.VERTICAL_ALIGNMENT_TOP,
+            height: "900px",
+            text: this.logManager.recentLogsAsText()
+        })
     }
 
 
