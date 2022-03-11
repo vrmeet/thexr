64805494fb6773cdab04b0139a9453e59d9dca4d
Homan Chou <homanchou@gmail.com>
2022-03-10 18:57:01 -0800

    session 98: vr menu positioning

--- assets/js/menu/menu-manager.ts
+++ assets/js/menu/menu-manager.ts
@@ -96,9 +96,10 @@ export class MenuManager {
         this.wristPlane = BABYLON.MeshBuilder.CreatePlane("wrist_plane", { height: 0.1, width: 0.1 }, this.scene)
         BABYLON.Tags.AddTagsTo(this.wristPlane, "vr_menu_gui")
         this.wristPlane.showBoundingBox = true
-        this.wristPlane.position.z = 0.05
-        this.wristPlane.position.x = 0.05
-        this.wristPlane.position.y = 0.08
+        // this.wristPlane.position.z = 0.1
+        // this.wristPlane.position.x = 0.05
+        this.wristPlane.position.y = 0.05
+        this.wristPlane.rotation.x = 1
 
 
         this.wristPlane.parent = this.sceneManager.xrManager.left_input_source.grip
@@ -107,8 +108,9 @@ export class MenuManager {
         this.browsePlane = BABYLON.MeshBuilder.CreatePlane("browse_plane", { height: 1, width: 1 }, this.scene)
         BABYLON.Tags.AddTagsTo(this.browsePlane, "vr_menu_gui")
         this.browsePlane.showBoundingBox = true
-        this.browsePlane.position.z = 0.1
+        this.browsePlane.position.z = 0.6
         this.browsePlane.position.y = 0.2
+        this.browsePlane.rotation.x = 1.06
         this.browsePlane.parent = this.sceneManager.xrManager.left_input_source.grip
         BABYLON.Tags.AddTagsTo(this.browsePlane.parent, "vr_menu_gui")
 
