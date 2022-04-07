9e805b78ed4069934e95037aea76307de4e4b3d0
Homan Chou <homanchou@gmail.com>
2022-04-07 00:09:20 -0700

    Session 125: Snapshot component data

--- lib/thexr/snapshot.ex
+++ lib/thexr/snapshot.ex
@@ -1,6 +1,7 @@
 defmodule Thexr.Snapshot do
   import Ecto.Query, warn: false
   alias Thexr.Repo
+  alias Ecto.Multi
 
   alias Thexr.Spaces.Entity
 
@@ -16,21 +17,38 @@ defmodule Thexr.Snapshot do
     Enum.each(events, fn event -> process(space_id, event.type, event) end)
   end
 
-  def process(space_id, "entity_created", %{payload: %{"id" => id, "type" => type, "name" => name, "components" => components}}) do
-    attrs = %{id: id, space_id: space_id, name: name, type: type, components: components}
-    attrs = Entity.build_default_components(attrs)
-
-      changeset =
-        %Entity{}
-        |> Entity.changeset(attrs)
-
-      {:ok, %{entity: entity}} =
-        Multi.new()
-        |> Multi.insert(:entity, changeset)
-        |> Repo.transaction()
-
-      {:ok, entity}
-
-    end
+  def process(_space_id, "member_entered", _) do
   end
+
+  def process(space_id, "entity_created", %{
+        payload: payload
+      }) do
+    changeset =
+      %Entity{space_id: space_id}
+      |> Entity.changeset(payload)
+
+    Multi.new()
+    |> Multi.insert(:entity, changeset)
+    |> Repo.transaction()
+
+    # Repo.insert(changeset)
+  end
+
+  # def process(space_id, "entity_created", %{payload: %{"id" => id, "type" => type, "name" => name, "components" => components}}) do
+  #   attrs = %{id: id, space_id: space_id, name: name, type: type, components: components}
+  #   attrs = Entity.build_default_components(attrs)
+
+  #     changeset =
+  #       %Entity{}
+  #       |> Entity.changeset(attrs)
+
+  #     {:ok, %{entity: entity}} =
+  #       Multi.new()
+  #       |> Multi.insert(:entity, changeset)
+  #       |> Repo.transaction()
+
+  #     {:ok, entity}
+
+  #   end
+  # end
 end
---
+++ test/thexr/snapshot_test.exs
@@ -0,0 +1,49 @@
+defmodule Thexr.SnapshotTest do
+  use Thexr.DataCase
+  import Thexr.SpacesFixtures
+
+  alias Thexr.Spaces
+  alias Thexr.Snapshot
+  alias Thexr.Spaces.Entity
+  alias Thexr.Repo
+
+  test "creates a snapshot" do
+    space = space_fixture()
+    Thexr.SpaceSupervisor.start_space(space)
+
+    events = [
+      %{
+        "m" => "member_entered",
+        "p" => %{"member_id" => "jsdfk", "pos_rot" => %{}},
+        "ts" => 23423
+      },
+      %{
+        "m" => "member_entered",
+        "p" => %{"member_id" => "jsdfk", "pos_rot" => %{}},
+        "ts" => 34_789_723
+      },
+      %{
+        "m" => "entity_created",
+        "p" => %{
+          "type" => "box",
+          "id" => "029b7351-98be-4183-8848-579fab7cf5c7",
+          "name" => "mybox",
+          "components" => [
+            %{"type" => "position", "data" => [1, 2, 3]},
+            %{"type" => "rotation", "data" => [2, 3, 5]},
+            %{"type" => "scaling", "data" => [1, 1, 1]}
+          ]
+        },
+        "ts" => 2_423_423
+      }
+    ]
+
+    Enum.each(events, fn payload ->
+      Thexr.SpaceServer.process_event(space.slug, payload, self())
+    end)
+
+    Process.sleep(50)
+    Snapshot.update_snapshot(space.id)
+    Spaces.serialize(space) |> IO.inspect()
+  end
+end
