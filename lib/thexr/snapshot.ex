4b818f28b1e18e4b77a9c10bf4274c8ab1064476
Homan Chou <homanchou@gmail.com>
2022-04-06 23:14:03 -0700

    Session: 124 start snapshot

---
+++ lib/thexr/snapshot.ex
@@ -0,0 +1,36 @@
+defmodule Thexr.Snapshot do
+  import Ecto.Query, warn: false
+  alias Thexr.Repo
+
+  alias Thexr.Spaces.Entity
+
+  alias Thexr.Spaces
+
+  def clear_snapshot(space_id) do
+    query = from(e in Entity, where: e.space_id == ^space_id)
+    Repo.delete_all(query)
+  end
+
+  def update_snapshot(space_id, last_sequence \\ 0) do
+    events = Spaces.event_stream(space_id, last_sequence)
+    Enum.each(events, fn event -> process(space_id, event.type, event) end)
+  end
+
+  def process(space_id, "entity_created", %{payload: %{"id" => id, "type" => type, "name" => name, "components" => components}}) do
+    attrs = %{id: id, space_id: space_id, name: name, type: type, components: components}
+    attrs = Entity.build_default_components(attrs)
+
+      changeset =
+        %Entity{}
+        |> Entity.changeset(attrs)
+
+      {:ok, %{entity: entity}} =
+        Multi.new()
+        |> Multi.insert(:entity, changeset)
+        |> Repo.transaction()
+
+      {:ok, entity}
+
+    end
+  end
+end
