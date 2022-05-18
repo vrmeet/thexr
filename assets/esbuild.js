3402d848e0f09c073d488b0f75f8adb329fb7b5d
Homan Chou <homanchou@gmail.com>
2022-05-18 14:44:56 -0700

    Session 161: Add navigation mesh to project

--- assets/esbuild.js
+++ assets/esbuild.js
@@ -42,7 +42,7 @@ const globals = {
 
 // Define esbuild options + extras for watch and deploy
 let opts = {
-    entryPoints: ['js/app.js', 'js/orchestrator.ts'],
+    entryPoints: ['js/app.js', 'js/orchestrator.ts', 'js/navMeshWorker.js'],
     bundle: true,
     external: ["/fonts/*", "/images/*"],
     logLevel: 'info',
