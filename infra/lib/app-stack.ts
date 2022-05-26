42af6a140cf0e5611bd2a08d34a7f37a36eefb48
Homan Chou <homanchou@gmail.com>
2022-05-26 10:01:49 -0700

    some clean-up get sqs to lambda to dynamo save working

--- infra/lib/app-stack.ts
+++ infra/lib/app-stack.ts
@@ -3,8 +3,9 @@ import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
-import * as events from 'aws-cdk-lib/aws-events'
-import * as targets from 'aws-cdk-lib/aws-events-targets'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+// import * as events from 'aws-cdk-lib/aws-events'
+// import * as targets from 'aws-cdk-lib/aws-events-targets'
 import * as path from 'path';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as eventsources from 'aws-cdk-lib/aws-lambda-event-sources';
@@ -16,14 +17,6 @@ export class AppStack extends Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
-    // const bus = new events.EventBus(this, 'eventbus', {
-    //   eventBusName: 'ThexrEventBus'
-    // });
 
     // create a queue
 
@@ -32,20 +25,16 @@ export class AppStack extends Stack {
       fifo: true
     });
 
+    // create dynamodb
 
-    // create a rule to pipe events to SQS
+    const table = new dynamodb.Table(this, 'thexr-eventstreams', {
+      partitionKey: { name: 'space_id', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'sequence_range', type: dynamodb.AttributeType.STRING },
+      tableName: "thexr-event-streams",
+      timeToLiveAttribute: "TTL"
+    });
 
-    // const rule = new events.Rule(this, 'event_to_sqs', {
-    //   ruleName: "events-to-sqs-rule",
-    //   eventPattern: {
-    //     source: ["dev", "prod"]
-    //   },
-    //   eventBus: bus,
-    //   description: "send Thexr events to SQS"
-    // });
-
-    // rule.addTarget(new targets.SqsQueue(eventQueue));
-
+    // create lambda
 
     const lambdaDynamoEventWriter = new NodejsFunction(this, 'ThexrDynamoEventWriter', {
       memorySize: 1024,
@@ -54,10 +43,15 @@ export class AppStack extends Stack {
       handler: 'handler',
       functionName: 'write-events-to-dynamo',
       entry: path.join(__dirname, `./thexr-dynamo-event-writer/index.ts`),
+      environment: {
+        tableName: table.tableName
+      }
     });
 
+    // event source for lambda
     lambdaDynamoEventWriter.addEventSource(new eventsources.SqsEventSource(eventQueue));
 
+    table.grantReadWriteData(lambdaDynamoEventWriter)
 
   }
 }
