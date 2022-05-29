d8ec37bbf5d0711a803bac580b13b90f2b9e5317
Homan Chou <homanchou@gmail.com>
2022-05-28 18:37:28 -0700

    Session 170: change strategy to back up events using s3 instead

--- lib/thexr/event_writer.ex
+++ lib/thexr/event_writer.ex
@@ -1,6 +1,8 @@
 defmodule Thexr.EventWriter do
   use GenStage
 
+  @archive_chuck_size 100
+
   def start_link([]) do
     GenStage.start_link(__MODULE__, :ok, name: __MODULE__)
   end
@@ -8,9 +10,8 @@ defmodule Thexr.EventWriter do
   def init(:ok) do
     client = create_aws_client()
     env_name = Application.get_env(:thexr, :environment_name)
-    sqs_url = System.get_env("SQS_FIFO_EVENT_QUEUE_URL")
 
-    {:consumer, %{aws_client: client, env_name: env_name, sqs_url: sqs_url},
+    {:consumer, %{aws_client: client, env_name: env_name},
      subscribe_to: [{Thexr.QueueBroadcaster, max_demand: 5, min_demand: 2}]}
   end
 
@@ -27,54 +28,22 @@ defmodule Thexr.EventWriter do
     # TODO: write all events in bulk
 
     Enum.map(events, fn event ->
-      # Thexr.Spaces.create_event(event)
+      Thexr.Spaces.create_event(event)
+
+      if rem(event.sequence, @archive_chuck_size) == 0 do
+        Thexr.Spaces.batch_archive_eventstream_to_s3(
+          event.space_id,
+          event.sequence,
+          @archive_chuck_size,
+          state.aws_client
+        )
+      end
+
       # side-effect of processing each event into a snapshot
       Thexr.Snapshot.process(event.space_id, event.type, event)
-      Thexr.Spaces.set_max_event_sequence(event.space_id, event.sequence)
-      # return value is a entry for eventbridge
-      # create_sqs_entry(
-      #   "#{event.space_id}##{event.sequence}",
-      #   Jason.encode!(event),
-      #   event.space_id
-      # )
-
-      if state.aws_client != nil do
-        input = %{
-          "QueueUrl" => state.sqs_url,
-          "MessageBody" => Jason.encode!(event),
-          "MessageDeduplicationId" => event.sequence,
-          "MessageGroupId" => event.space_id
-        }
-
-        AWS.SQS.send_message(state.aws_client, input)
-      end
     end)
-
-    # |> Enum.chunk_every(10, 10, [])
-    # |> Enum.each(fn batch ->
-    #   #   IO.inspect(batch, label: "batch")
-    #   input = %{
-    #     "Entries" => Jason.encode!(batch),
-    #     "QueueUrl" => state.sqs_url
-    #   }
-
-    #   IO.inspect(input, label: "input")
-
-    #   AWS.SQS.send_message_batch(state.aws_client, input) |> IO.inspect(label: "send_message")
-    # end)
 
     # We are a consumer, so we would never emit items.
     {:noreply, [], state}
-  end
-
-  # id - identifies the unique message
-  # message_body json string
-  def create_sqs_entry(id, message_body, group_id) do
-    %{
-      "Id" => id,
-      "MessageBody" => message_body,
-      "MessageDeduplicationId" => group_id,
-      "MessageGroupId" => group_id
-    }
   end
 end
