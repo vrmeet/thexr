bc0db2392e14fc7bdcdc8af93b00724bf375ad3d
Homan Chou <homanchou@gmail.com>
2022-06-23 19:45:34 -0700

    try fix another crash

--- lib/event_name.ex
+++ lib/event_name.ex
@@ -24,6 +24,7 @@ defmodule EventName do
   def int_to_atom(6000), do: :hud_message_broadcasted
   def int_to_atom(7000), do: :agent_spawned
   def int_to_atom(7100), do: :agent_directed
+  def int_to_atom(8000), do: :target_hit
 
   def atom_to_int(:member_entered), do: 1000
   def atom_to_int(:member_left), do: 1010
@@ -45,6 +46,7 @@ defmodule EventName do
   def atom_to_int(:hud_message_broadcasted), do: 6000
   def atom_to_int(:agent_spawned), do: 7000
   def atom_to_int(:agent_directed), do: 7100
+  def atom_to_int(:target_hit), do: 8000
 
 end
         
