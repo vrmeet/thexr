b91ec82278282fd32a090cf224ed54d78ad08e51
Homan Chou <homanchou@gmail.com>
2022-06-29 23:22:09 -0700

    Session 198: Global editing mode and GUI to toggle value

--- assets/js/menu/pages/main.ts
+++ assets/js/menu/pages/main.ts
@@ -1,19 +1,38 @@
 import type * as BABYLON from 'babylonjs'
 import * as GUI from 'babylonjs-gui'
 import { signalHub } from '../../signalHub';
+import { mode } from "../../mode"
 import { span, a, div, toggle } from '../helpers';
 
 export class MenuPageMain extends GUI.Container {
     constructor(public scene: BABYLON.Scene) {
         super()
 
+        const editToggle = toggle({ value: (mode.editing) ? 1 : 0 }) as GUI.Slider
+        editToggle.onValueChangedObservable.add(data => {
+            console.log(data)
+            if (data > 0.5) {
+                mode.editing = true
+            } else {
+                mode.editing = false
+            }
+            signalHub.menu.emit("menu_topic", "main")
+        })
+
+        let row;
+        if (mode.editing) {
+            row = span({}, editToggle, "Editing", a({ callback: this.cb("tools") }, "Tools"))
+        } else {
+            row = span({}, editToggle, "Editing")
+        }
 
         this.addControl(
             div({ name: "main-page-container" },
                 "Main Menu",
                 a({ callback: this.cb("about") }, "About"),
                 a({ callback: this.cb("logs") }, "Logs"),
-                a({ callback: this.cb("tools") }, "Tools"),
+                row,
+
 
             ))
     }
